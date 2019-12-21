87df657e1c3d9d594bd80476fc1d5766919f783c
saydy guzman <saidyguzman.20@gmail.com>
2019-12-21 10:58:28 -0500

    version1

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const morgan =require ('morgan');
+const cors  = require ('cors');
+
+const {mongoose} = require ('./database');
+//Settings
+app.set('port',process.env.PORT || 3000);
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors({origin:'http://localhost:4200'}));
+
+
+//Routes
+app.use('/api/employees',require('./routers/employee.routes'));
+
+
+//Starting the server
+
+app.listen(app.get('port'), () =>{
+    console.log('Server on port', app.get('port'));
+});
